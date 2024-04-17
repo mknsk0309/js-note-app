1523fa4020b1c44eca38f994f4da3e1be4702fe2
mknsk0309 <156224339+mknsk0309@users.noreply.github.com>
2024-04-17 02:31:26 +0000

    [update] セクション完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Main from './components/Main'
 import Sidebar from './components/Sidebar'
@@ -6,13 +6,25 @@ import uuid from 'react-uuid'
 
 function App() {
 
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem('notes')) || []
+  )
+  const [activeNote, setActiveNote] = useState(false)
+
+  useEffect(() => {
+    // ローカルストレージにノートを保存する
+    localStorage.setItem('notes', JSON.stringify(notes))
+  }, [notes])
+
+  useEffect(() => {
+    setActiveNote(notes[0].id)
+  }, [])
 
   const onAddNote = () => {
     const newNote = {
       id: uuid(),
       title: '新しいノート',
-      content: '新しいノートの内容',
+      content: '',
       modDate: Date.now()
     }
     setNotes([...notes, newNote])
@@ -24,10 +36,33 @@ function App() {
     setNotes(filterNotes)
   }
 
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote)
+  }
+
+  const onUpdateNote = (updatedNote) => {
+    // 修正された新しいノートの配列を返す。
+    const updatedNotesArray = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote
+      } else {
+        return note
+      }
+    })
+
+    setNotes(updatedNotesArray)
+  }
+
   return (
     <div className='App'>
-      <Sidebar onAddNote={onAddNote} notes={notes} onDeleteNote={onDeleteNote} />
-      <Main />
+      <Sidebar
+        onAddNote={onAddNote}
+        notes={notes}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+        />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
     </div>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './Main.css'
+import Markdown from 'react-markdown'
 
-const Main = () => {
+const Main = ({ activeNote, onUpdateNote }) => {
+
+  const onEditNote = (key, value) => {
+    onUpdateNote({
+      ...activeNote,
+      [key]: value,
+      modDate: Date.now(),
+    })
+  }
+
+  if (!activeNote) {
+    return (
+      <div className="no-active-note">ノートが選択されていません。</div>
+    )
+  }
+
   return (
-    <div>Main</div>
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input
+          id='title'
+          type='text'
+          value={activeNote.title}
+          onChange={(e) => onEditNote("title", e.target.value)}
+        />
+        <textarea
+          id='content'
+          placeholder='ノートの内容を記入'
+          value={activeNote.content}
+          onChange={(e) => onEditNote("content", e.target.value)}
+        ></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h3 className="preview-title">{activeNote.title}</h3>
+        <Markdown className="markdown-preview">
+          {activeNote.content}
+        </Markdown>
+      </div>
+    </div>
   )
 }
 
